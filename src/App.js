f8c5ecb2d5158a677a65e428a3410f5bfa3e409a
zzanguna <yajung0203@naver.com>
2020-02-20 14:26:44 +0900

    함수형 컴포넌트 정의 : 주로 출력만 하는 경우에 사용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import MyComponent from './components/MyComponent';
+import MyComponentFunc from './components/MyComponentFunc';
 
 function App() {
   return ( 
     <React.Fragment>
-       <MyComponent name={3} number={5} />
+       <MyComponent name={"정윤아"} number={5} />
 
-      <MyComponent/>
+      <MyComponent name={"짱유나"} number={55}/>
+      <MyComponentFunc name={"정윤아"} number={5} />
     </React.Fragment>
   );
 }
---
+++ src/components/MyComponentFunc.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyComponentFunc = ({name, number}) => {
+    return(
+        <div>
+            <h2>I'm 함수형 컴포넌트 {name}  / {number}</h2>
+        </div>
+    )
+}
+
+export default MyComponentFunc;
