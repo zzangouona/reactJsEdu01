5758ee0008dc62e686e3515f997e1331bb8d6cc7
zzanguna <yajung0203@naver.com>
2020-02-20 13:41:01 +0900

    props : 부모가 전달해주는 변수

---
+++ src/components/MyComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+    render() {
+        const {name} = this.props;
+
+        return (
+            <div>
+                {/* <h1>내 이름은 {this.props.name}</h1> */}
+                <h1>내 이름은 {name}</h1>
+            </div>
+        );
+    }
+}
+
+export default MyComponent;//다른 파일에 import 시키기 위해 허용해준다.
