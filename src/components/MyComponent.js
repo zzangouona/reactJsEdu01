56b1456bcbfa1e52fc9d24ebf580bbfce79abb65
zzanguna <yajung0203@naver.com>
2020-02-20 15:12:08 +0900

    onChange 핸들링

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
     state = {
-        number:0
+        number: 0,
+        message: ""
     }
 
     //handleIncrease 함수
     //기능 : state.number 변수를 증가해준다.
     handleIncrease = () => {
         this.setState({
-            number : this.state.number +1
+            number: this.state.number + 1
         })
     }
 
@@ -18,19 +19,24 @@ class MyComponent extends Component {
     //기능 : state.number 변수를 감소해준다.
     handleDecrease = () => {
         this.setState({
-            number : this.state.number -1
+            number: this.state.number - 1
         })
     }
 
     render() {
-        const {name, value} = this.props;
-        const {number} = this.state;
-        const {handleDecrease, handleIncrease} = this;
-        
+        const { name, value } = this.props;
+        const { number, message } = this.state;
+        const { handleDecrease, handleIncrease } = this;
+
         return (
             <div>
                 {/* <h1>내 이름은 {this.props.name}</h1> */}
                 <h1>내 이름은 {name}/{value}</h1>
+                <p>입력된 Message 값은 {message}</p>
+                <input type="text" value = {message} 
+                onChange={(e)=> (this.setState({
+                    message: e.target.value
+                }))} />
                 <p>Number 값 {number}</p>
                 <button onClick={handleIncrease}>+</button>
                 <button onClick={handleDecrease}>-</button>
@@ -39,7 +45,7 @@ class MyComponent extends Component {
     }
 }
 MyComponent.defaultProps = {
-    name : "디폴트값"
+    name: "디폴트값"
 };
 
 MyComponent.propTypes = {
